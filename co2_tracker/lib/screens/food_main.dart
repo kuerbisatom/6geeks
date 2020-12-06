79a81586ae3b993e72c3e1e8d415bf5198acd135
patriciapiedade <patricia.piedade@ymail.com>
2020-12-06 23:05:09 +0000

    refined look for food tracking

--- co2_tracker/lib/screens/food_main.dart
+++ co2_tracker/lib/screens/food_main.dart
@@ -1,4 +1,5 @@
 import 'package:co2_tracker/screens/home_screen.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:searchable_dropdown/searchable_dropdown.dart';
 
@@ -28,48 +29,63 @@ class _FoodMainState extends State<FoodMain> {
   Widget build(BuildContext context) {
     return new Scaffold(
       body: Center(
-        child: Column(
-        children: [Icon(Icons.local_dining_outlined),
-                  Text('Food'),
-                  Text('Search Item'),
-                  SearchableDropdown.multiple(
-                    items: items.map((item) {
-                      return new DropdownMenuItem<listItem>(
-                          child: Text(item.name), value: item);
-                    }).toList(),
-                    selectedItems: selectedItems,
-                    hint: Padding(
-                      padding: const EdgeInsets.all(12.0),
-                      child: Text("Select any"),
-                    ),
-                    searchHint: "Select any",
-                    onChanged: (value) {
-                      setState(() {
-                        selectedItems = value;
-                      });
-                    },
-                    closeButton: (selectedItems) {
-                      return (selectedItems.isNotEmpty
-                          ? "Save ${selectedItems.length == 1 ? '"' + items[selectedItems.first].value.toString() + '"' : '(' + selectedItems.length.toString() + ')'}"
-                          : "Save without selection");
-                    },
-                    isExpanded: true,
-                  ),
 
-                  Text('Scan Item'),
-                  ElevatedButton(
-                    onPressed: () {
-                      // TODO: Navigate to Item scanning mock
-                    },
-                    child: Icon(Icons.qr_code_scanner),
+        child: Column(
+        children: [
+                  Container(
+                    padding: EdgeInsets.all(30.0),
+                    child:  SearchableDropdown.multiple(
+                      items: items.map((item) {
+                        return new DropdownMenuItem<listItem>(
+                            child: Text(item.name), value: item);
+                            }).toList(),
+                            selectedItems: selectedItems,
+                            hint: Padding(
+                              padding: const EdgeInsets.all(12.0),
+                              child: Text("Select items",
+                                style: TextStyle(fontSize: 20),),
+                            ),
+                            searchHint: "Select items",
+                            onChanged: (value) {
+                              setState(() {
+                                selectedItems = value;
+                              });
+                            },
+                            closeButton: (selectedItems) {
+                              return (selectedItems.isNotEmpty
+                                  ? "Save ${selectedItems.length == 1 ? '"' + items[selectedItems.first].value.toString() + '"' : '(' + selectedItems.length.toString() + ')'}"
+                                  : "Save without selection");
+                            },
+                            isExpanded: true,
+                    ),
                   ),
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(context, MaterialPageRoute(builder: (context) => MyHomePage()));
-                      // TODO Navigate home screen and add itens to emissions
-                    },
-                    child: Text('Add All'),
-                  )
+                  Text('Scan Item',
+                  style: TextStyle(fontSize: 20),),
+                  Container(
+                    child: ElevatedButton(
+                      onPressed: () {
+                        // TODO: Navigate to Item scanning mock
+                      },
+                      child: Icon(Icons.qr_code_scanner,
+                      size:60,
+                        ),
+                    ),
+                    width: 100,
+                    height: 100,
+                    margin: EdgeInsets.all(20),
+                  ),
+                  Container(
+                      child: ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(context, MaterialPageRoute(builder: (context) => MyHomePage()));
+                          // TODO Navigate home screen and add itens to emissions
+                        },
+                        child: Text('Add All',
+                          style: TextStyle(fontSize: 30),),
+                      ),
+                    padding: EdgeInsets.all(20),
+                    margin: EdgeInsets.only(top:20),
+                  ),
         ],
 
       ))
